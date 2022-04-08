417fcea7a7c3fa380cd80a3300ac83afbf7b5830
Aaronhdez <aaronhdezalv@hotmail.com>
2022-04-08 18:38:04 +0100

    refactor: player spawn now works properly

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] public List<GameObject> spawnPoints;
-    [SerializeField] public GameObject player; 
+    [SerializeField] public GameObject player;
+
     void Start() {
-        var randomPick = Random.Range(0, spawnPoints.Count + 1);
-        var spawnPoint = spawnPoints[randomPick].transform;
-        Instantiate(player, spawnPoint.position, 
-            spawnPoint.rotation, spawnPoint);
-        player.SetActive(true);
+        RespawnPlayer();
+    }
+
+    private void RespawnPlayer() {
+        var randomPick = Random.Range(0, spawnPoints.Count);
+        var spawnPoint = spawnPoints[randomPick].transform.position;
+        player.transform.position = new Vector3(spawnPoint.x,
+            spawnPoint.y, spawnPoint.z);
     }
 }
