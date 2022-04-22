445043d2c9cc06375c58b7ed7648fa1e59036cd5
Aaronhdez <aaronhdezalv@hotmail.com>
2022-04-22 11:12:24 +0100

    green: basic HudController added to project

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
+
+    [Header("Player")]
     [SerializeField] public GameObject player;
     [SerializeField] public List<GameObject> enemies;
     [SerializeField] public GameObject spawnManager;
+    [SerializeField] public GameObject hudManager;
 
     void Start() {
         spawnManager.GetComponent<SpawnManager>().RespawnPlayer(player);
         spawnManager.GetComponent<SpawnManager>().RespawnEnemies(enemies);
-        //spawnManager.GetComponent<SpawnManager>().RespawnProps(enemies);
+        hudManager.GetComponent<HudController>().PresetHUD();
     }
 
-    
+    public void RestorePlayerHealth() {
+        hudManager.GetComponent<HudController>().ReloadHealthHud();
+        Debug.Log("Health Restored");
+    }
+
+    public void RestorePlayerAmmo() {
+        hudManager.GetComponent<HudController>().ReloadAmmoHuds();
+        Debug.Log("Ammo added to Gun");
+    }
 }
