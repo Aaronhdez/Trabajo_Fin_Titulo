1aaafe0377c53b43c761a6f1cca240b574572a0e
Aaronhdez <aaronhdezalv@hotmail.com>
2022-04-22 12:44:01 +0100

    green: HealthController added to player

--- Assets/Scripts/Player/HealthController.cs
+++ Assets/Scripts/Player/HealthController.cs
@@ -3,24 +3,45 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthController : MonoBehaviour
-{
+public class HealthController : MonoBehaviour {
+    
+    [Header("Health Presets")]
     public int maxHealth = 500;
     public int currentHealth = 500;
     public bool isDead = false;
+
+    [Header("Elements Associated")]
     public GameObject hudElementAssociated;
     public TextMeshProUGUI healthText;
     public Image healthImage;
     private static readonly Color goodHeathColor = new Color(0.3f, 1f, 0f, 0.6f);
-    private static readonly Color normalHeathColor = new Color(0.3f, 1f, 0f, 0.6f);
-    private static readonly Color badHeathColor = new Color(0.3f, 1f, 0f, 0.6f);
+    private static readonly Color normalHeathColor = new Color(1f, 0.6f, 0f, 0.6f);
+    private static readonly Color badHeathColor = new Color(1f, 0f, 0f, 0.6f);
 
     void Start() {
         UpdateHealthHud();
     }
 
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.H)) {
+            if(currentHealth > 0) { 
+                currentHealth -= (currentHealth >= 25) ?
+                    25 :
+                    currentHealth;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.J)) {
+            if (currentHealth < maxHealth) {
+                currentHealth += (currentHealth <= maxHealth - 25) ?
+                    25 :
+                    (maxHealth - currentHealth);
+            }
+        }
+        UpdateHealthHud();
+    }
+
     private void UpdateHealthHud() {
-        float percentage = currentHealth / maxHealth;
+        float percentage = (float) currentHealth / maxHealth;
         if (percentage > 0.66f) {
             healthText.color = goodHeathColor;
             healthImage.color = goodHeathColor;
@@ -31,6 +52,7 @@ public class HealthController : MonoBehaviour
             healthText.color = normalHeathColor;
             healthImage.color = normalHeathColor;
         }
+        healthText.SetText((percentage * 100)+"%");
     }
 
     internal void RestorePlayerHealth() {
