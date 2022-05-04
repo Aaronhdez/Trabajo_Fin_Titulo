36de573c0b492153d0c3378e4128676efc0c6b69
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-04 18:16:14 +0100

    green: Enemy_health_is_decreased_if_damage_is_applied

--- Assets/Tests/EnemyControllerTest.cs
+++ Assets/Tests/EnemyControllerTest.cs
@@ -9,13 +9,14 @@ namespace Tests {
         [SetUp]
         public void SetUp() {
             enemy = Instantiate(
-                Resources.Load<GameObject>("Prefabs/Characters/Characters/Enemy1_V2"));
+                Resources.Load<GameObject>("Prefabs/Characters/V2_NavMeshes/Enemy1_V2"));
             enemyController = enemy.GetComponent<EnemyController>();
         }
 
         [Test]
         public void Enemy_health_is_decreased_if_damage_is_applied() {
             var health = enemyController.health;
+            enemyController.ApplyDamage(10);
             Assert.AreNotEqual(health, enemyController.health);
         }
     }
