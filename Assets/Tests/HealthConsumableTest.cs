b4273c904c86fe385cba70b803456287ef632729
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-05 19:02:09 +0100

    refactor: Test refactored to include setUp variables

--- Assets/Tests/HealthConsumableTest.cs
+++ Assets/Tests/HealthConsumableTest.cs
@@ -1,29 +1,36 @@
 using NUnit.Framework;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Tests {
     public class HealthConsumableTest {
-        [Test]
-        public void Player_health_is_restored_when_health_consumable_is_used() { 
-            var medKit = MonoBehaviour.Instantiate(
+        private GameObject medKit;
+        private GameObject player;
+        private GameObject healthController;
+        private HealthConsumable consumableController;
+        private PlayerController playerController;
+
+        [SetUp]
+        public void SetUp(){
+            medKit = MonoBehaviour.Instantiate(
                 Resources.Load<GameObject>("Prefabs/Supply/Consumables/MedKitProp"));
-            var player = MonoBehaviour.Instantiate(
+            player = MonoBehaviour.Instantiate(
                 Resources.Load<GameObject>("Prefabs/Characters/PlayerPrefab"));
-            var healthController = MonoBehaviour.Instantiate(
+            healthController = MonoBehaviour.Instantiate(
                 Resources.Load<GameObject>("Prefabs/Characters/PlayerAssets/HealthController"));
-            var consumableController = medKit.GetComponent<HealthConsumable>();
-            var playerController = player.GetComponent<PlayerController>();
+            consumableController = medKit.GetComponent<HealthConsumable>();
+            playerController = player.GetComponent<PlayerController>();
             consumableController.player = player;
             playerController.healthController = healthController.GetComponent<HealthController>();
+        }
+
+        [Test]
+        public void Player_health_is_restored_when_health_consumable_is_used() { 
             playerController.healthController.maxHealth = 100;
             playerController.healthController.currentHealth = 50;
 
             consumableController.ApplyEffectOver();
-
-            var newHealth = playerController.healthController.currentHealth;
-            Assert.AreEqual(playerController.healthController.currentHealth, newHealth);
+            Assert.AreEqual(playerController.healthController.maxHealth,
+                playerController.healthController.currentHealth);
         }
     }
 }
