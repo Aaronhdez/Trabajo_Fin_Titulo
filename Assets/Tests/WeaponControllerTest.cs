98a4d6637aebd7bcf634d3abec301a58cf293f78
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-05 17:44:33 +0100

    refactor: Test refactored

--- Assets/Tests/WeaponControllerTest.cs
+++ Assets/Tests/WeaponControllerTest.cs
@@ -5,77 +5,68 @@ using UnityEngine;
 
 namespace Tests {
     public class WeaponControllerTest : MonoBehaviour {
+        private GameObject weapon;
+        private WeaponController weaponController;
+        private int amountInMagazine;
+
         [SetUp]
+        public void SetUp() {
+            weapon = Instantiate(
+                Resources.Load<GameObject>("Prefabs/Weapons/Rifle"));
+            weaponController = weapon.GetComponent<WeaponController>();
+        }
 
         [Test]
         public void Weapon_ammo_quantity_is_reduced_when_weapon_is_fired() {
-            GameObject weapon = Instantiate(
-                Resources.Load<GameObject>("Prefabs/Weapons/Rifle"));
-            var weaponController = weapon.GetComponent<WeaponController>();
-            var amountInMagazine = weaponController.amountInMagazine;
-
+            amountInMagazine = weaponController.amountInMagazine;
             weaponController.Fire();
+
             Assert.AreNotEqual(amountInMagazine, weaponController.amountInMagazine);
         }
 
         [Test]
         public void Weapon_ammo_quantity_is_restored_when_weapon_is_reloaded() {
-            GameObject weapon = Instantiate(
-                Resources.Load<GameObject>("Prefabs/Weapons/Rifle"));
-            var weaponController = weapon.GetComponent<WeaponController>();
-            var amountInMagazine = weaponController.amountInMagazine;
-
+            amountInMagazine = weaponController.amountInMagazine;
             weaponController.Fire();
             weaponController.Reload();
+
             Assert.AreEqual(amountInMagazine, weaponController.amountInMagazine);
         }
 
         [Test]
         public void Weapon_total_ammo_quantity_is_reduced_when_weapon_is_reloaded() {
-            GameObject weapon = Instantiate(
-                Resources.Load<GameObject>("Prefabs/Weapons/Rifle"));
-            var weaponController = weapon.GetComponent<WeaponController>();
-            var amountInMagazine = weaponController.amountOfBullets;
-
+            amountInMagazine = weaponController.amountOfBullets;
             weaponController.Fire();
             weaponController.Reload();
+
             Assert.AreNotEqual(amountInMagazine, weaponController.amountOfBullets);
         }
 
         [Test]
         public void Weapon_total_ammo_quantity_is_reduced_to_zero_on_last_reload() {
-            GameObject weapon = Instantiate(
-                Resources.Load<GameObject>("Prefabs/Weapons/Rifle"));
-            var weaponController = weapon.GetComponent<WeaponController>();
             weaponController.amountOfBullets = 30;
             weaponController.amountInMagazine = 10;
             weaponController.maxAmountInMagazine = 50;
-
             weaponController.Reload();
+
             Assert.AreEqual(weaponController.amountOfBullets, 0);
         }
 
         [Test]
         public void Weapon_total_ammo_quantity_is_restored_to_max_on_restoration() {
-            GameObject weapon = Instantiate(
-                Resources.Load<GameObject>("Prefabs/Weapons/Rifle"));
-            var weaponController = weapon.GetComponent<WeaponController>();
             weaponController.amountOfBullets = 30;
             weaponController.maxAmountOfBullets = 500;
-
             weaponController.RestoreAmmoValues();
+
             Assert.AreEqual(weaponController.amountOfBullets, weaponController.maxAmountOfBullets);
         }
 
         [Test]
         public void Weapon_total_ammo_quantity_in_magazine_is_restored_to_max_on_restoration() {
-            GameObject weapon = Instantiate(
-                Resources.Load<GameObject>("Prefabs/Weapons/Rifle"));
-            var weaponController = weapon.GetComponent<WeaponController>();
             weaponController.amountInMagazine = 25;
             weaponController.maxAmountInMagazine = 30;
-
             weaponController.RestoreAmmoValues();
+
             Assert.AreEqual(weaponController.amountInMagazine, weaponController.maxAmountInMagazine);
         }
     }
