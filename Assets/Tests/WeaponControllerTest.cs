436c860225965bc7479dab1dac642b93d8237e99
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-05 17:22:38 +0100

    red: Weapon_ammo_quantity_is_restored_when_weapon_is_reloaded

--- Assets/Tests/WeaponControllerTest.cs
+++ Assets/Tests/WeaponControllerTest.cs
@@ -17,5 +17,16 @@ namespace Tests {
             weaponController.Fire();
             Assert.AreNotEqual(amountInMagazine, weaponController.amountInMagazine);
         }
+
+        [Test]
+        public void Weapon_ammo_quantity_is_restored_when_weapon_is_reloaded() {
+            GameObject weapon = Instantiate(
+                Resources.Load<GameObject>("Prefabs/Weapons/Rifle"));
+            var weaponController = weapon.GetComponent<WeaponController>();
+            var amountInMagazine = weaponController.amountInMagazine;
+
+            weaponController.Fire();
+            Assert.AreEqual(amountInMagazine, weaponController.amountInMagazine);
+        }
     }
 }
