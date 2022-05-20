1f44bc2ad95d3b325168a5e9994aa04962e66bc9
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-20 18:50:58 +0100

    green: Agent_should_wander_if_the_rest_of_possibilites_are_not_possible

--- Assets/Tests/BT/Trees/BT_DummyBT_V1_Test.cs
+++ Assets/Tests/BT/Trees/BT_DummyBT_V1_Test.cs
@@ -73,4 +73,37 @@ public class BT_DummyBT_V1_Test : MonoBehaviour {
         wanderAround.Verify(c => c.Evaluate(), Times.Never());
         checkAttackRange.Verify(c => c.Evaluate(), Times.AtLeastOnce());
     }
+
+    [Test]
+    public void Agent_should_wander_if_the_rest_of_possibilites_are_not_possible() {
+        var checkAttackRange = new Mock<CheckTargetIsInAttackRange>();
+        var attack = new Mock<Attack>();
+        var checkFOVRange = new Mock<CheckTargetIsInFOVRange>();
+        var chase = new Mock<Chase>();
+        var wanderAround = new Mock<WanderAround>();
+
+        INode root = new Selector(new List<Node>() {
+                new Sequence(new List<Node>() {
+                    checkAttackRange.Object,
+                    attack.Object
+                }),
+                new Sequence(new List<Node>() {
+                    checkFOVRange.Object,
+                    chase.Object
+                }),
+                wanderAround.Object
+            });
+
+        dummyBT = new DummyBT_V1(root);
+
+        checkAttackRange.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
+        checkFOVRange.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
+
+        dummyBT.Start();
+        dummyBT.Update();
+
+        checkAttackRange.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+        checkFOVRange.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+        wanderAround.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+    }
 }
