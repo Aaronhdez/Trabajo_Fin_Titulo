0f17e93985a6e9035b3bbcb28e90587935f75c0e
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-20 16:27:54 +0100

    refactor: tests refactored to TestCases

--- Assets/Tests/BT/BT_SelectorTest.cs
+++ Assets/Tests/BT/BT_SelectorTest.cs
@@ -5,55 +5,41 @@ using System.Collections.Generic;
 
 namespace Tests {
     public class BT_SelectorTest {
-        [Test]
-        public void Selector_returns_failure_on_both_child_failure() {
+
+        private List<Mock<Node>> children2Node;
+        private List<Mock<Node>> children3Node;
+
+        [SetUp]
+        public void Setup() {
             Mock<Node> child1 = new Mock<Node>();
             Mock<Node> child2 = new Mock<Node>();
-
-            List<Node> children = new List<Node>();
-            children.Add(child1.Object);
-            children.Add(child2.Object);
-
-            Selector selector = new Selector(children);
-
-            child1.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
-            child1.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
-
-            Assert.AreEqual(NodeState.FAILURE, selector.Evaluate());
+            Mock<Node> child3 = new Mock<Node>();
+            children2Node = new List<Mock<Node>> {
+                child1,
+                child2
+            };
+            children3Node = new List<Mock<Node>> {
+                child1,
+                child2,
+                child3
+            };
         }
 
-        [Test]
-        public void Selector_returns_success_on_one_child_success() {
-            Mock<Node> child1 = new Mock<Node>();
-            Mock<Node> child2 = new Mock<Node>();
-
+        [TestCase(NodeState.FAILURE, NodeState.FAILURE, NodeState.FAILURE)]
+        [TestCase(NodeState.FAILURE, NodeState.SUCCESS, NodeState.SUCCESS)]
+        [TestCase(NodeState.SUCCESS, NodeState.FAILURE, NodeState.SUCCESS)]
+        [TestCase(NodeState.FAILURE, NodeState.RUNNING, NodeState.RUNNING)]
+        [TestCase(NodeState.RUNNING, NodeState.FAILURE, NodeState.RUNNING)]
+        public void Selector_test_cases_for_two_nodes(NodeState state1, NodeState state2, NodeState result) {
             List<Node> children = new List<Node>();
-            children.Add(child1.Object);
-            children.Add(child2.Object);
-
+            children.Add(children2Node[0].Object);
+            children.Add(children2Node[1].Object);
             Selector selector = new Selector(children);
 
-            child1.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
-            child1.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
+            children2Node[0].Setup(c => c.Evaluate()).Returns(state1);
+            children2Node[1].Setup(c => c.Evaluate()).Returns(state2);
 
-            Assert.AreEqual(NodeState.SUCCESS, selector.Evaluate());
-        }
-
-        [Test]
-        public void Selector_returns_running_on_one_child_running() {
-            Mock<Node> child1 = new Mock<Node>();
-            Mock<Node> child2 = new Mock<Node>();
-
-            List<Node> children = new List<Node>();
-            children.Add(child1.Object);
-            children.Add(child2.Object);
-
-            Selector selector = new Selector(children);
-
-            child1.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
-            child1.Setup(c => c.Evaluate()).Returns(NodeState.RUNNING);
-
-            Assert.AreEqual(NodeState.RUNNING, selector.Evaluate());
+            Assert.AreEqual(result, selector.Evaluate());
         }
 
         [Test]
