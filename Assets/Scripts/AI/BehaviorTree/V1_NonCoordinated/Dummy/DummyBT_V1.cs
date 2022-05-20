a61091b8946a80fddbe4d5adf546ccc85e64ae19
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-20 18:27:13 +0100

    refactor: added custom constructor in DummyBT class for testing purposes

--- Assets/Scripts/AI/BehaviorTree/V1_NonCoordinated/Dummy/DummyBT_V1.cs
+++ Assets/Scripts/AI/BehaviorTree/V1_NonCoordinated/Dummy/DummyBT_V1.cs
@@ -6,8 +6,17 @@ namespace BehaviorTree {
     public class DummyBT_V1 : Tree {
 
         public GameObject agent;
+        private INode _customRoot = null;
+
+        public DummyBT_V1(INode root) {
+            _customRoot = root;
+        }
 
         protected override INode SetupTree() {
+            if(_customRoot != null) {
+                return _customRoot;
+            }
+
             INode root = new Selector(new List<Node>() {
                 new Sequence(new List<Node>() {
                     new CheckTargetIsInAttackRange(agent),
