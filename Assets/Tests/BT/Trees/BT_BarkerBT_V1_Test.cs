5018d0500287781b94defc22feb4443e9389c518
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-20 20:11:58 +0100

    refactor: tests refactored to avoid duplicated code

--- Assets/Tests/BT/Trees/BT_BarkerBT_V1_Test.cs
+++ Assets/Tests/BT/Trees/BT_BarkerBT_V1_Test.cs
@@ -17,52 +17,8 @@ namespace Tests.BehaviourTrees.V1 {
         private Mock<Chase> chase;
         private Mock<WanderAround> wanderAround;
 
-
-        [Test]
-        public void Barker_should_alert_if_conditions_are_met() {
-            checkAlertNotTriggered = new Mock<CheckIfAlertIsNotTriggered>();
-            checkFOVRange_Alert = new Mock<CheckTargetIsInFOVRange>();
-            alert = new Mock<CheckIfAlertIsNotTriggered>();
-            checkAttackRange = new Mock<CheckTargetIsInAttackRange>();
-            attack = new Mock<Attack>();
-            checkFOVRange_Chase = new Mock<CheckTargetIsInFOVRange>();
-            chase = new Mock<Chase>();
-            wanderAround = new Mock<WanderAround>();
-
-            INode root = new Selector(new List<Node>() {
-                new Sequence(new List<Node>(){ 
-                    checkAlertNotTriggered.Object,
-                    checkFOVRange_Alert.Object,
-                    alert.Object
-                }),
-                new Sequence(new List<Node>() {
-                    checkAttackRange.Object,
-                    attack.Object
-                }),
-                new Sequence(new List<Node>() {
-                    checkFOVRange_Chase.Object,
-                    chase.Object
-                }),
-                wanderAround.Object
-            });
-
-            barkerBT = new BarkerBT_V1(root);
-
-            checkAlertNotTriggered.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
-            checkFOVRange_Alert.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
-            alert.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
-
-            barkerBT.InitTree();
-            barkerBT.UpdateNodes();
-
-            checkAlertNotTriggered.Verify(c => c.Evaluate(), Times.AtLeastOnce());
-            checkFOVRange_Alert.Verify(c => c.Evaluate(), Times.AtLeastOnce());
-            alert.Verify(c => c.Evaluate(), Times.AtLeastOnce());
-            checkAttackRange.Verify(c => c.Evaluate(), Times.Never());
-        }
-
-        [Test]
-        public void Barker_should_attack_if_conditions_are_met() {
+        [SetUp]
+        public void SetUp() {
             checkAlertNotTriggered = new Mock<CheckIfAlertIsNotTriggered>();
             checkFOVRange_Alert = new Mock<CheckTargetIsInFOVRange>();
             alert = new Mock<CheckIfAlertIsNotTriggered>();
@@ -90,7 +46,25 @@ namespace Tests.BehaviourTrees.V1 {
             });
 
             barkerBT = new BarkerBT_V1(root);
+        }
 
+        [Test]
+        public void Barker_should_alert_if_conditions_are_met() {
+            checkAlertNotTriggered.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
+            checkFOVRange_Alert.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
+            alert.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
+
+            barkerBT.InitTree();
+            barkerBT.UpdateNodes();
+
+            checkAlertNotTriggered.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+            checkFOVRange_Alert.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+            alert.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+            checkAttackRange.Verify(c => c.Evaluate(), Times.Never());
+        }
+
+        [Test]
+        public void Barker_should_attack_if_conditions_are_met() {
             checkAlertNotTriggered.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
             checkAttackRange.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
             attack.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
@@ -107,34 +81,6 @@ namespace Tests.BehaviourTrees.V1 {
 
         [Test]
         public void Barker_should_chase_if_conditions_are_met() {
-            checkAlertNotTriggered = new Mock<CheckIfAlertIsNotTriggered>();
-            checkFOVRange_Alert = new Mock<CheckTargetIsInFOVRange>();
-            alert = new Mock<CheckIfAlertIsNotTriggered>();
-            checkAttackRange = new Mock<CheckTargetIsInAttackRange>();
-            attack = new Mock<Attack>();
-            checkFOVRange_Chase = new Mock<CheckTargetIsInFOVRange>();
-            chase = new Mock<Chase>();
-            wanderAround = new Mock<WanderAround>();
-
-            INode root = new Selector(new List<Node>() {
-                new Sequence(new List<Node>(){
-                    checkAlertNotTriggered.Object,
-                    checkFOVRange_Alert.Object,
-                    alert.Object
-                }),
-                new Sequence(new List<Node>() {
-                    checkAttackRange.Object,
-                    attack.Object
-                }),
-                new Sequence(new List<Node>() {
-                    checkFOVRange_Chase.Object,
-                    chase.Object
-                }),
-                wanderAround.Object
-            });
-
-            barkerBT = new BarkerBT_V1(root);
-
             checkAlertNotTriggered.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
             checkAttackRange.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
             checkFOVRange_Chase.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
@@ -154,34 +100,6 @@ namespace Tests.BehaviourTrees.V1 {
 
         [Test]
         public void Barker_should_wander_if_conditions_are_met() {
-            checkAlertNotTriggered = new Mock<CheckIfAlertIsNotTriggered>();
-            checkFOVRange_Alert = new Mock<CheckTargetIsInFOVRange>();
-            alert = new Mock<CheckIfAlertIsNotTriggered>();
-            checkAttackRange = new Mock<CheckTargetIsInAttackRange>();
-            attack = new Mock<Attack>();
-            checkFOVRange_Chase = new Mock<CheckTargetIsInFOVRange>();
-            chase = new Mock<Chase>();
-            wanderAround = new Mock<WanderAround>();
-
-            INode root = new Selector(new List<Node>() {
-                new Sequence(new List<Node>(){
-                    checkAlertNotTriggered.Object,
-                    checkFOVRange_Alert.Object,
-                    alert.Object
-                }),
-                new Sequence(new List<Node>() {
-                    checkAttackRange.Object,
-                    attack.Object
-                }),
-                new Sequence(new List<Node>() {
-                    checkFOVRange_Chase.Object,
-                    chase.Object
-                }),
-                wanderAround.Object
-            });
-
-            barkerBT = new BarkerBT_V1(root);
-
             checkAlertNotTriggered.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
             checkAttackRange.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
             checkFOVRange_Chase.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
