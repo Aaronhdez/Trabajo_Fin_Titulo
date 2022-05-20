e813001c29a54c5b07d64f3f74337f2aa113b9c7
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-20 16:58:10 +0100

    red: Returns_running_with_at_least_one_running

--- Assets/Tests/BT/BT_SequenceTest.cs
+++ Assets/Tests/BT/BT_SequenceTest.cs
@@ -26,5 +26,25 @@ namespace Tests {
 
             Assert.AreEqual(NodeState.FAILURE, selector.Evaluate());
         }
+
+        [Test]
+        public void Returns_running_with_at_least_one_running() {
+            Mock<Node> child1 = new Mock<Node>();
+            Mock<Node> child2 = new Mock<Node>();
+            children2Node = new List<Mock<Node>> {
+                child1,
+                child2
+            };
+
+            List<Node> children = new List<Node>();
+            children.Add(children2Node[0].Object);
+            children.Add(children2Node[1].Object);
+            Sequence selector = new Sequence(children);
+
+            children2Node[0].Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
+            children2Node[1].Setup(c => c.Evaluate()).Returns(NodeState.RUNNING);
+
+            Assert.AreEqual(NodeState.RUNNING, selector.Evaluate());
+        }
     }
 }
