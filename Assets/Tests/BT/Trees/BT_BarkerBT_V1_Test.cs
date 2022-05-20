ef9755d092fb506295ba40ec59acbd435382c780
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-20 20:06:59 +0100

    green: Barker_should_attack_if_conditions_are_met

--- Assets/Tests/BT/Trees/BT_BarkerBT_V1_Test.cs
+++ Assets/Tests/BT/Trees/BT_BarkerBT_V1_Test.cs
@@ -60,5 +60,49 @@ namespace Tests.BehaviourTrees.V1 {
             alert.Verify(c => c.Evaluate(), Times.AtLeastOnce());
             checkAttackRange.Verify(c => c.Evaluate(), Times.Never());
         }
+
+        [Test]
+        public void Barker_should_attack_if_conditions_are_met() {
+            checkAlertNotTriggered = new Mock<CheckIfAlertIsNotTriggered>();
+            checkFOVRange_Alert = new Mock<CheckTargetIsInFOVRange>();
+            alert = new Mock<CheckIfAlertIsNotTriggered>();
+            checkAttackRange = new Mock<CheckTargetIsInAttackRange>();
+            attack = new Mock<Attack>();
+            checkFOVRange_Chase = new Mock<CheckTargetIsInFOVRange>();
+            chase = new Mock<Chase>();
+            wanderAround = new Mock<WanderAround>();
+
+            INode root = new Selector(new List<Node>() {
+                new Sequence(new List<Node>(){
+                    checkAlertNotTriggered.Object,
+                    checkFOVRange_Alert.Object,
+                    alert.Object
+                }),
+                new Sequence(new List<Node>() {
+                    checkAttackRange.Object,
+                    attack.Object
+                }),
+                new Sequence(new List<Node>() {
+                    checkFOVRange_Chase.Object,
+                    chase.Object
+                }),
+                wanderAround.Object
+            });
+
+            barkerBT = new BarkerBT_V1(root);
+
+            checkAlertNotTriggered.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
+            checkAttackRange.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
+            attack.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
+
+            barkerBT.InitTree();
+            barkerBT.UpdateNodes();
+
+            checkAlertNotTriggered.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+            checkFOVRange_Alert.Verify(c => c.Evaluate(), Times.Never());
+            alert.Verify(c => c.Evaluate(), Times.Never());
+            checkAttackRange.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+            attack.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+        }
     }
 }
