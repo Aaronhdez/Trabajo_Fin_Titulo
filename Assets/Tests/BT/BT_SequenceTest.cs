48ac587496c3fc7afc93d1335d9de18d4cf715ff
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-20 17:05:05 +0100

    refactor: tests refactored to TestCases

--- Assets/Tests/BT/BT_SequenceTest.cs
+++ Assets/Tests/BT/BT_SequenceTest.cs
@@ -6,65 +6,42 @@ using System.Collections.Generic;
 namespace Tests {
     public class BT_SequenceTest {
         private List<Mock<Node>> children2Node;
+        private List<Mock<Node>> children3Node;
 
-        [Test]
-        public void Returns_failure_with_at_least_one_failure() {
+        [SetUp]
+        public void Setup() {
             Mock<Node> child1 = new Mock<Node>();
             Mock<Node> child2 = new Mock<Node>();
+            Mock<Node> child3 = new Mock<Node>();
             children2Node = new List<Mock<Node>> {
                 child1,
                 child2
             };
-
-            List<Node> children = new List<Node>();
-            children.Add(children2Node[0].Object);
-            children.Add(children2Node[1].Object);
-            Sequence selector = new Sequence(children);
-
-            children2Node[0].Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
-            children2Node[1].Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
-
-            Assert.AreEqual(NodeState.FAILURE, selector.Evaluate());
+            children3Node = new List<Mock<Node>> {
+                child1,
+                child2,
+                child3
+            };
         }
 
-        [Test]
-        public void Returns_running_with_at_least_one_running() {
-            Mock<Node> child1 = new Mock<Node>();
-            Mock<Node> child2 = new Mock<Node>();
-            children2Node = new List<Mock<Node>> {
-                child1,
-                child2
-            };
-
+        [TestCase(NodeState.FAILURE, NodeState.FAILURE, NodeState.FAILURE)]
+        [TestCase(NodeState.FAILURE, NodeState.SUCCESS, NodeState.FAILURE)]
+        [TestCase(NodeState.SUCCESS, NodeState.FAILURE, NodeState.FAILURE)]
+        [TestCase(NodeState.FAILURE, NodeState.RUNNING, NodeState.FAILURE)]
+        [TestCase(NodeState.RUNNING, NodeState.FAILURE, NodeState.FAILURE)]
+        [TestCase(NodeState.RUNNING, NodeState.SUCCESS, NodeState.RUNNING)]
+        [TestCase(NodeState.SUCCESS, NodeState.RUNNING, NodeState.RUNNING)]
+        [TestCase(NodeState.SUCCESS, NodeState.SUCCESS, NodeState.SUCCESS)]
+        public void Sequence_test_cases_for_two_nodes(NodeState state1, NodeState state2, NodeState result) {
             List<Node> children = new List<Node>();
             children.Add(children2Node[0].Object);
             children.Add(children2Node[1].Object);
             Sequence selector = new Sequence(children);
 
-            children2Node[0].Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
-            children2Node[1].Setup(c => c.Evaluate()).Returns(NodeState.RUNNING);
+            children2Node[0].Setup(c => c.Evaluate()).Returns(state1);
+            children2Node[1].Setup(c => c.Evaluate()).Returns(state2);
 
-            Assert.AreEqual(NodeState.RUNNING, selector.Evaluate());
-        }
-
-        [Test]
-        public void Returns_success_every_node_has_finished_as_success() {
-            Mock<Node> child1 = new Mock<Node>();
-            Mock<Node> child2 = new Mock<Node>();
-            children2Node = new List<Mock<Node>> {
-                child1,
-                child2
-            };
-
-            List<Node> children = new List<Node>();
-            children.Add(children2Node[0].Object);
-            children.Add(children2Node[1].Object);
-            Sequence selector = new Sequence(children);
-
-            children2Node[0].Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
-            children2Node[1].Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
-
-            Assert.AreEqual(NodeState.SUCCESS, selector.Evaluate());
+            Assert.AreEqual(result, selector.Evaluate());
         }
     }
 }
