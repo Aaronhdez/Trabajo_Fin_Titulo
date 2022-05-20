2e188c426f9ca335e897212a7c536a4e9a683437
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-20 15:51:46 +0100

    red: Selector_returns_success_on_one_child_success

--- Assets/Tests/BT/BT_SelectorTest.cs
+++ Assets/Tests/BT/BT_SelectorTest.cs
@@ -22,5 +22,23 @@ namespace Tests {
             Assert.AreEqual(NodeState.FAILURE, selector.Evaluate());
         }
 
+
+        [Test]
+        public void Selector_returns_success_on_one_child_success() {
+            Mock<Node> child1 = new Mock<Node>();
+            Mock<Node> child2 = new Mock<Node>();
+
+            List<Node> children = new List<Node>();
+            children.Add(child1.Object);
+            children.Add(child2.Object);
+
+            Selector selector = new Selector(children);
+
+            child1.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
+            child1.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
+
+            Assert.AreEqual(NodeState.SUCCESS, selector.Evaluate());
+        }
+
     }
 }
