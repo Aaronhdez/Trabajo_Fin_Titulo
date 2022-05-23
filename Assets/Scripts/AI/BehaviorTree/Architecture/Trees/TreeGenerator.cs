87210171d01dafbb848caeddab882fd5a69a55e0
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-23 17:51:15 +0100

    refactor: added Dummy_V2 to tree generator

--- Assets/Scripts/AI/BehaviorTree/Architecture/Trees/TreeGenerator.cs
+++ Assets/Scripts/AI/BehaviorTree/Architecture/Trees/TreeGenerator.cs
@@ -10,6 +10,7 @@ namespace BehaviorTree {
         public TreeGenerator(GameObject agent) {
             treesAvaliable = new Dictionary<string, Tree>();
             treesAvaliable.Add("DummyBT_V1", new DummyBT_V1(null, agent));
+            treesAvaliable.Add("DummyBT_V2", new DummyBT_V1(null, agent));
             treesAvaliable.Add("BarkerBT_V1", new BarkerBT_V1(null, agent));
         }
 
