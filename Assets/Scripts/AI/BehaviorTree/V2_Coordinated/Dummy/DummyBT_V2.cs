e5f0d4c2830bee3c71a4542012911d6fdf44572d
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-23 17:35:31 +0100

    green: Dummy_V2_should_die_if_the_agent_is_dead

---
+++ Assets/Scripts/AI/BehaviorTree/V2_Coordinated/Dummy/DummyBT_V2.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorTree {
+    public class DummyBT_V2 : Tree {
+
+        private INode _customRoot = null;
+
+        public DummyBT_V2(INode root, GameObject agent) {
+            _customRoot = root;
+            _agent = agent;
+        }
+
+        protected override INode SetupTree() {
+            if (_customRoot != null) {
+                return _customRoot;
+            }
+
+            INode root = new Selector(new List<Node>() {
+                new Sequence(new List<Node>(){
+                    new CheckIfAgentIsDead(_agent),
+                    new Dead(_agent)
+                }),
+                new Sequence(new List<Node>() {
+                    new CheckTargetIsInAttackRange(_agent),
+                    new Attack(_agent)
+                }),
+                new Selector(new List<Node>() {
+                    new Sequence(new List<Node>() {
+                        new CheckIfAlertIsNotTriggered(_agent),
+                        new CheckIfAgentCanBeAlerted(_agent),
+                        new ChaseOnAlert(_agent)
+                    }),
+                    new Sequence(new List<Node>() {
+                        new CheckTargetIsInFOVRange(_agent),
+                        new Chase(_agent)
+                    }),
+                }),
+                new WanderAround(_agent)
+            });
+
+            return root;
+        }
+    }
+}
---
+++ Assets/Tests/BT/Trees/BT_DummyBT_V2_Test.cs
@@ -0,0 +1,125 @@
+using BehaviorTree;
+using Moq;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tests.BehaviourTrees.V2 {
+    public class BT_DummyBT_V2_Test {
+        private ITree dummyBT;
+        private Mock<CheckIfAgentIsDead> checkAgentIsDead;
+        private Mock<Dead> dead;
+        private Mock<CheckTargetIsInAttackRange> checkAttackRange;
+        private Mock<Attack> attack;
+        private Mock<CheckIfAlertIsNotTriggered> checkIfAlertIsNotTriggered;
+        private Mock<CheckIfAgentCanBeAlerted> checkIfAgentCanBeAlerted;
+        private Mock<ChaseOnAlert> chaseOnAlert;
+        private Mock<CheckTargetIsInFOVRange> checkFOVRange;
+        private Mock<Chase> chase;
+        private Mock<WanderAround> wanderAround;
+
+        [SetUp]
+        public void SetUp() {
+            checkAgentIsDead = new Mock<CheckIfAgentIsDead>();
+            dead = new Mock<Dead>();
+
+            checkAttackRange = new Mock<CheckTargetIsInAttackRange>();
+            attack = new Mock<Attack>();
+
+            checkIfAlertIsNotTriggered = new Mock<CheckIfAlertIsNotTriggered>();
+            checkIfAgentCanBeAlerted = new Mock<CheckIfAgentCanBeAlerted>();
+            chaseOnAlert = new Mock<ChaseOnAlert>();
+
+            checkFOVRange = new Mock<CheckTargetIsInFOVRange>();
+            chase = new Mock<Chase>();
+
+            wanderAround = new Mock<WanderAround>();
+
+            INode root = new Selector(new List<Node>() {
+                new Sequence(new List<Node>() {
+                    checkAgentIsDead.Object,
+                    dead.Object
+                }),
+                new Sequence(new List<Node>() {
+                    checkAttackRange.Object,
+                    attack.Object
+                }),
+                new Sequence(new List<Node>() {
+                    checkFOVRange.Object,
+                    chase.Object
+                }),
+
+                new Selector(new List<Node>() {
+                    new Sequence(new List<Node>() {
+                        checkIfAlertIsNotTriggered.Object,
+                        checkIfAgentCanBeAlerted.Object,
+                        chaseOnAlert.Object
+                    }),
+                    new Sequence(new List<Node>() {
+                        checkFOVRange.Object,
+                        chase.Object
+                    }),
+                }),
+                wanderAround.Object
+            });
+
+            dummyBT = new DummyBT_V2(root, null);
+        }
+
+        [Test]
+        public void Dummy_V2_should_die_if_the_agent_is_dead() {
+            checkAgentIsDead.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
+            dead.Setup(c => c.Evaluate()).Returns(NodeState.RUNNING);
+
+            dummyBT.InitTree();
+            dummyBT.UpdateNodes();
+
+            checkAgentIsDead.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+            dead.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+            checkAttackRange.Verify(c => c.Evaluate(), Times.Never());
+        }
+
+        /*[Test]
+        public void Dummy_should_attack_if_conditions_are_met() {
+            checkAgentIsDead.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
+            checkAttackRange.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
+            attack.Setup(c => c.Evaluate()).Returns(NodeState.RUNNING);
+
+            dummyBT.InitTree();
+            dummyBT.UpdateNodes();
+
+            checkAttackRange.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+            attack.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+            checkFOVRange.Verify(c => c.Evaluate(), Times.Never());
+        }
+
+        [Test]
+        public void Dummy_should_chase_if_attacking_is_not_possible() {
+            checkAgentIsDead.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
+            checkAttackRange.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
+            checkFOVRange.Setup(c => c.Evaluate()).Returns(NodeState.SUCCESS);
+            chase.Setup(c => c.Evaluate()).Returns(NodeState.RUNNING);
+
+            dummyBT.InitTree();
+            dummyBT.UpdateNodes();
+
+            wanderAround.Verify(c => c.Evaluate(), Times.Never());
+            checkAttackRange.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+        }
+
+        [Test]
+        public void Dummy_should_wander_if_the_rest_of_possibilites_are_not_possible() {
+            checkAgentIsDead.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
+            checkAttackRange.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
+            checkFOVRange.Setup(c => c.Evaluate()).Returns(NodeState.FAILURE);
+
+            dummyBT.InitTree();
+            dummyBT.UpdateNodes();
+
+            checkAttackRange.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+            checkFOVRange.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+            wanderAround.Verify(c => c.Evaluate(), Times.AtLeastOnce());
+        }*/
+    }
+}
