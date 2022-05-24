2b454a36de78643d1e43504771114201616c42eb
Aaronhdez <aaronhdezalv@hotmail.com>
2022-05-24 17:32:48 +0100

    refactor: Alert manager refactroed to permit target triangulation

--- Assets/Scripts/Mechanics/AlertController.cs
+++ Assets/Scripts/Mechanics/AlertController.cs
@@ -1,6 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Mechanics {
@@ -23,8 +22,21 @@ namespace Mechanics {
                 _lastTriggertime = currentTriggertime;
                 AlertManager.SetActiveStatusTo(true);
                 AlertManager.SetLastAlertPosition(transform.position);
+                AlertManager.SetLastZoneReported(TriangulatePlayerPosition(transform.position));
                 Debug.Log("Ultima posición del juagdor: " + AlertManager.GetLastAlertPosition());
             }
+        }
+
+        private List<Vector3> TriangulatePlayerPosition(Vector3 position) {
+            var layerMask = (1 << 16);
+            var alertPoints = Physics.OverlapSphere(position, 30f, layerMask);
+            alertPoints.OrderBy(x => Vector3.Distance(x.transform.position, position));
+            List<Collider> filteredPoints = new List<Collider>(alertPoints);
+            List<Vector3> lastZone = new List<Vector3>();
+            foreach (var filteredPoint in filteredPoints.GetRange(0, 3)) {
+                lastZone.Add(filteredPoint.transform.position);
+            }
+            return lastZone;
         }
     }
 }
