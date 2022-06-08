9bf1bf40a4a0374a1d5cd5b2688253376176e72c
Aaronhdez <aaronhdezalv@hotmail.com>
2022-06-08 18:44:00 +0100

    green: flow options added to game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,8 +6,42 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     [SerializeField] private RoundManager roundManager;
+    [SerializeField] private SceneSelector sceneSelector;
+    private bool isGamePaused = false;
 
     void Start() {
         roundManager = GetComponent<RoundManager>();
+        sceneSelector = GetComponent<SceneSelector>();
+    }
+
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            isGamePaused = !isGamePaused;
+            PauseGame();
+        }
+    }
+
+    public void FinishGame() {
+        roundManager.FinishGame();
+    }
+
+    public void RestartGame() {
+        sceneSelector.PlayGame();
+    }
+
+    public void BackToMenu() {
+        sceneSelector.GoToMainMenu();
+    }
+
+    public void PauseGame() {
+        if (isGamePaused) {
+            roundManager.PauseRound();
+            isGamePaused = true;
+            Time.timeScale = 0;
+        } else {
+            roundManager.ResumeRound();
+            isGamePaused = false;
+            Time.timeScale = 1;
+        }
     }
 }
