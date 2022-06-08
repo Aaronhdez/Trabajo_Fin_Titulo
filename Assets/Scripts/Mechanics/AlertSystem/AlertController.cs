fdd491cff94e30f241a4f9982ca369c555815367
Aaronhdez <aaronhdezalv@hotmail.com>
2022-06-08 18:14:17 +0100

    refactor: AlertControllers refactored to a unique class

--- Assets/Scripts/Mechanics/AlertSystem/AlertController.cs
+++ Assets/Scripts/Mechanics/AlertSystem/AlertController.cs
@@ -1,12 +1,14 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 namespace Mechanics {
-    public class AlertController_NearestNodes : MonoBehaviour {
+    public class AlertController : MonoBehaviour{
         public float _alertPeriod;
         private float _lastTriggertime = 0f;
         public bool _AlertTriggered;
+        public bool furthestNodes;
 
         public void Update() {
             var currentTriggertime = Time.time;
@@ -22,22 +24,37 @@ namespace Mechanics {
             var currentTriggertime = Time.time;
             if (!AlertManager.AlertHasBeenTriggered()) {
                 _lastTriggertime = currentTriggertime;
-                AlertManager.ResetPositions();
-                AlertManager.SetActiveStatusTo(true);
-                AlertManager.SetLastAlertPosition(transform.position);
-                TriangulatePlayerPosition(transform.position);
-                Debug.Log("Ultima posición del juagdor: " + AlertManager.GetLastAlertPosition());
+                UpdatePositions();
             }
+        }
+
+        public void UpdatePositions() {
+            AlertManager.ResetPositions();
+            AlertManager.SetActiveStatusTo(true);
+            AlertManager.SetLastAlertPosition(transform.position);
+            TriangulatePlayerPosition(transform.position);
         }
 
         private void TriangulatePlayerPosition(Vector3 position) {
             var layerMask = (1 << 16);
-            var alertPoints = Physics.OverlapSphere(position, 30f, layerMask);
-            alertPoints.OrderBy(x => Vector3.Distance(x.transform.position, position));
-            List<Collider> filteredPoints = new List<Collider>(alertPoints);
+            var alertPoints = Physics.OverlapSphere(position, 50f, layerMask);
+            List<Collider> filteredPoints = GetOrderedPoints(position, alertPoints);
+            UpdateZones(filteredPoints);
+        }
 
-            List<Vector3> lastZone = new List<Vector3>();
+        private List<Collider> GetOrderedPoints(Vector3 position, Collider[] alertPoints) {
+            if (furthestNodes) {
+                alertPoints.OrderByDescending(x => Vector3.Distance(x.transform.position, position));
+            } else {
+                alertPoints.OrderBy(x => Vector3.Distance(x.transform.position, position));
+            }
+            List<Collider> filteredPoints = new List<Collider>(alertPoints);
+            return filteredPoints;
+        }
+
+        private static void UpdateZones(List<Collider> filteredPoints) {
             List<GameObject> lastZoneInNodes = new List<GameObject>();
+            List<Vector3> lastZone = new List<Vector3>();
 
             foreach (var filteredPoint in filteredPoints.GetRange(0, 3)) {
                 lastZone.Add(filteredPoint.transform.position);
