76a36485a42eea3ad11fe1fccade0b5f03bec290
Aaronhdez <aaronhdezalv@hotmail.com>
2022-06-14 16:17:56 +0100

    refactor: test strcuture refactored

--- Assets/Tests/BT/Nodes/Bot/BT_CheckIfThereAreDummiesNearby.cs
+++ Assets/Tests/BT/Nodes/Bot/BT_CheckIfThereAreDummiesNearby.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 namespace Tests.BehaviourTrees.Nodes.Bot {
     public class BT_CheckIfThereAreDummiesNearby {
-        [Test]
-        public void Returns_FAILURE_if_there_are_no_dummies_nearby() {
-            var agent = new GameObject();
+        private GameObject agent;
+        private CheckIfThereAreDummiesNearby node;
+        private GameObject dummyZombie;
+
+        [SetUp]
+        public void SetUp() {
+            agent = new GameObject();
             agent.AddComponent<BotController>();
             agent.GetComponent<BotController>().FovRange = 10;
             agent.transform.position = new Vector3(0, 0, 0);
-            var node = new CheckIfThereAreDummiesNearby(agent);
+            node = new CheckIfThereAreDummiesNearby(agent);
 
-            var dummyZombie = new GameObject();
+            dummyZombie = new GameObject();
+            dummyZombie.transform.position = new Vector3(0, 0, 12);
+        }
+
+        [Test]
+        public void Returns_FAILURE_if_there_are_no_dummies_nearby() {
             var dummyZombieLayer = 13;
             dummyZombie.layer = dummyZombieLayer;
-            dummyZombie.transform.position = new Vector3(0, 0, 12);
 
             Assert.AreEqual(NodeState.FAILURE, node.Evaluate());
         }
-
 
     }
 }
